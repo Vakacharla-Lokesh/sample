7ff48633997fbef3faa1797e421fe5b08fb9f5c9
Vakacharla-Lokesh <lokeshgpta04@gmail.com>
2023-02-06 19:35:26 +0530

    First commit

---
+++ practice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    cls();
+    printf("Welcome to my first program \n");
+    getch();
+    cls();
+    printf("My Name is Lokesh \n");
+    getch();
+    int clrscr();
+    printf("I am in class 12 \n");
+    getch();
+    return 0;
+}
